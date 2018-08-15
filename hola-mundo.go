c69b1caf1f40aaff83cb2fd0544cc6c7a2570ab5
Rodolfo Pilas <rodolfo@pilas.guru>
2018-08-15 16:55:21 -0300

    changes hola-mundo

--- hola-mundo.go
+++ hola-mundo.go
@@ -12,12 +12,12 @@ func main() {
 
 	var MiMac = Notebook{"Macbook", 17, true}
 	fmt.Println(MiMac.tmk)
-	/*
-		var suma int = 8 + 9
-		var resta int = 6 - 4
-		var nombre string = "Rodolfo"
-		fmt.Println("hola mundo desde Go con " + nombre)
-		fmt.Println(suma)
-		fmt.Println(resta)
-	*/
+
+	var suma int = 8 + 9
+	var resta int = 6 - 4
+	var nombre string = "Rodolfo"
+	fmt.Println("hola mundo desde Go con " + nombre)
+	fmt.Println(suma)
+	fmt.Println(resta)
+
 }
